4cac5144fa06339f4abdf1d2641831f37376df87
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-26 15:32:33 -0400

    Added PERPAGE variable to pass in

--- client/components/user_list.js
+++ client/components/user_list.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createContainer} from 'meteor/react-meteor-data';
 import {Users} from '../../imports/collections/users';
 import UserDetail from './user_detail';
+const PER_PAGE = 20:
 
 const UserList = (props) => {
   console.log(props.users)
@@ -10,13 +11,18 @@ const UserList = (props) => {
       <div className="user-list">
         {props.users.map(user => <UserDetail user={user} key={user._id} />)}
       </div>
+      <button onClick = {() => Meteor.subscribe('users', 40) }
+       className = "btn btn-lg btn-primary">
+        Load More..
+      </button>
+
     </div>
   );
 };
 
 export default createContainer(() => {
   //set up subsciption
-  Meteor.subscribe('users');
+  Meteor.subscribe('users', PER_PAGE);
 
   // return an object.  This is set to UserList as props
   return { users: Users.find({}).fetch() }
