91ea82b7beec4712f35a01d939ce39d908e79634
amansawhney <amansawhney@sawhneygames.com>
2016-07-26 19:38:05 -0400

    Refactored UserList to a class componet and added a function for the click handler

--- client/components/user_list.js
+++ client/components/user_list.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {createContainer} from 'meteor/react-meteor-data';
 import {Users} from '../../imports/collections/users';
 import UserDetail from './user_detail';
-const PER_PAGE = 20:
 
-const UserList = (props) => {
-  console.log(props.users)
-  return (
-    <div>
-      <div className="user-list">
-        {props.users.map(user => <UserDetail user={user} key={user._id} />)}
+const PER_PAGE = 20;
+
+class UserList extends Component {
+  handleButtonClick() {
+     Meteor.subscribe('users', 40)
+  }
+  render() {
+    return (
+      <div>
+        <div className="user-list">
+          {this.props.users.map(user => <UserDetail user={user} key={user._id} />)}
+        </div>
+        <button onClick = {handleButtonClick} className = "btn btn-lg btn-primary">
+          Load More..
+        </button>
+
       </div>
-      <button onClick = {() => Meteor.subscribe('users', 40) }
-       className = "btn btn-lg btn-primary">
-        Load More..
-      </button>
-
-    </div>
-  );
+    );
+}
 };
 
 export default createContainer(() => {
